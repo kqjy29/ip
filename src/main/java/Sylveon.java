4214fceba4a5466d3d634fac9785adcafd45c1a3
kaelin <kaekaequek@gmail.com>
2026-08-20 13:41:11 +0800

    Renamed chatbot from Duke to Sylveon

---
+++ src/main/java/Sylveon.java
@@ -0,0 +1,11 @@
+public class Sylveon {
+    public static void main(String[] args) {
+        String banner = "  ____          _                              \n"
+                + " / ___| _   _  | | __   __  ___   ___   _ __  \n"
+                + " \\___ \\| | | | | | \\ \\ / / / _ \\ / _ \\ | '_ \\ \n"
+                + "  ___) | |_| | | |  \\ V / |  __/| (_) || | | |\n"
+                + " |____/ \\__, | |_|   \\_/   \\___| \\___/ |_| |_|\n"
+                + "        |___/                                 \n";
+        System.out.println(banner);
+    }
+}
