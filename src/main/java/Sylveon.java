849940a4b1b8965cab4356f567c19ffce7263f83
kaelin <kaekaequek@gmail.com>
2026-08-20 13:58:32 +0800

    Completed  Level-0

--- src/main/java/Sylveon.java
+++ src/main/java/Sylveon.java
@@ -5,7 +5,13 @@ public class Sylveon {
                 + " \\___ \\| | | | | | \\ \\ / / / _ \\ / _ \\ | '_ \\ \n"
                 + "  ___) | |_| | | |  \\ V / |  __/| (_) || | | |\n"
                 + " |____/ \\__, | |_|   \\_/   \\___| \\___/ |_| |_|\n"
-                + "        |___/                                 \n";
+                + "        |___/                                 ";
+        String line = "----------<3----------<3----------<3----------";
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hi!! I'm Sylveon <3\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye bye :) Hope to see you again soon <3");
+        System.out.println(line);
     }
 }
