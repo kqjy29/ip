cdf9eec33fc7a4634996570dd147823aad2688a8
kaelin <kaekaequek@gmail.com>
2026-09-15 21:19:02 +0800

    Fix storage date parsing and add product screenshot

--- src/main/java/sylveon/Storage.java
+++ src/main/java/sylveon/Storage.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
